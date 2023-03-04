fdfb8bbc58b013aa4b4ed6239565ed1e6e472a49
lucboj <lucasbojer@gmail.com>
2023-03-04 14:58:29 +0100

    Restricted script

--- install-go-hdb.sh
+++ install-go-hdb.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script should only be used in Github Actions and is not made to be used for installing locally on computer
+
+if [ "$1" != "install" ];
+then
+    echo "Please do not use this script for installing HANA Go driver locally on computer. This is for Github Actions"
+    exit 1
+fi
+
 # Get GOROOT
 prefix="GOROOT=\""
 suffix="\""
